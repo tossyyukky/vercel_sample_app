8d9e5fa5b603d6db32038cabbf00bb669a1300a3
Toshiyuki Ihara <snowsphere.burned@gmail.com>
2021-06-26 16:28:43 +0900

    modify: text

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Vercel!!!!!</a>
+          Welcome to Vercel!!!!!
         </h1>
 
         <p className={styles.description}>
